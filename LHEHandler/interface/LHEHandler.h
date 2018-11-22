949b9300a0cbab1ea9aece7ee35587c68622d5a3
Ulascan Sarica <ulascan90@gmail.com>
2018-11-22 08:27:44 -0800

    Files

---
+++ LHEHandler/interface/LHEHandler.h
@@ -0,0 +1,79 @@
+/** \class LHEHandler
+ *
+ *
+ *  \author N. Amapane - Torino
+ *  \author U. Sarica - UCSB/JHU
+ *  \author H. Roskes - JHU
+ *
+ *
+ */
+#ifndef LHEHADNLER_H
+#define LHEHANDLER_H
+
+#include <DataFormats/Common/interface/Handle.h>
+#include <SimDataFormats/GeneratorProducts/interface/LHERunInfoProduct.h>
+#include <SimDataFormats/GeneratorProducts/interface/LHEEventProduct.h>
+#include <ZZMatrixElement/MELA/interface/Mela.h>
+#include <MelaAnalytics/EventContainer/interface/MELAEvent.h>
+
+#include <vector>
+
+
+class LHEHandler{
+public:
+
+  LHEHandler(int VVMode_, int VVDecayMode_, bool doKinematics_, int year_);
+  LHEHandler(edm::Handle<LHEEventProduct>* lhe_evt_, int VVMode_, int VVDecayMode_, bool doKinematics_, int year_);
+  virtual ~LHEHandler();
+  
+  void setHandle(edm::Handle<LHEEventProduct>* lhe_evt_);
+  void extract();
+  void clear();
+
+  MELACandidate* getBestCandidate();
+  float const& getLHEOriginalWeight() const; // Weight written in the <event> block, supposed to = genhepmcweight if no Pythia reweighting is done
+  float const& getPowhegOriginalWeight() const; // Weight from POWHEG before JHUGen reweighting, taken from alternate weight 1001.  If there are no alternate weights this is the same as the LHEOriginalWeight
+  float getLHEWeight(unsigned int whichWeight, float defaultValue=1) const; // = {Weights written in LHE weight variations} / getLHEOriginalWeight()
+  float getLHEWeight_PDFVariationUpDn(int whichUpDn, float defaultValue=1) const; // = {Weights written in LHE weight variations} / getLHEOriginalWeight()
+  float getLHEWeigh_AsMZUpDn(int whichUpDn, float defaultValue=1) const; // = {Weights written in LHE weight variations} / getLHEOriginalWeight()
+  float const& getPDFScale() const;
+  float reweightNNLOtoNLO() const;
+
+  static bool compareAbsIsLess(float val1, float val2);
+  static float findNearestOneSigma(float ref, int lowhigh, std::vector<float> const& wgt_array);
+
+protected:
+
+  // VVMode and VVDecayMode: See comment lines within MELAEvent::constructVVCandidates
+  const int VVMode;
+  const int VVDecayMode;
+  const bool doKinematics;
+  const int year;
+
+  edm::Handle<LHEEventProduct>* lhe_evt;
+  std::vector<MELAParticle*> particleList;
+  MELAEvent* genEvent;
+  MELACandidate* genCand;
+
+  float defaultNLOweight;
+  float LHEOriginalWeight;
+  float powhegOriginalWeight;
+  std::vector<float> LHEWeight;
+  std::vector<float> LHEWeight_PDFVariationUpDn;
+  std::vector<float> LHEWeight_AsMZUpDn;
+  std::vector<int> PDFid;
+  float PDFScale;
+
+  void readEvent();
+
+  enum AlternateWeightsType {
+    unknown,
+    powheg,
+    madgraph_0offset,
+    madgraph_1000offset
+  };
+
+};
+
+
+#endif
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# LHEHandler
-Common LHE options
+# CommonLHETools
+Common LHE tools for HIG and SUS analyses
+
+Checkout instructions:
+
+git cms-init
+
+# MELA Analytics
+git clone https://github.com/usarica/MelaAnalytics.git
+
+# MELA
+git clone https://github.com/cms-analysis/HiggsAnalysis-ZZMatrixElement.git ZZMatrixElement
+
+# Common LHE tools
+git clone https://github.com/usarica/CommonLHETools.git
